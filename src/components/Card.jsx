33883dfbe7e27c5d5b313beb68bea777217484f0
ardiansyahdodii <dodyn890@gmail.com>
2023-11-23 00:17:42 +0700

    perbaikan layout-2

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,7 +12,7 @@ const Card = (props) => {
                 className='h-[200px] md:h-[250px] w-[250px] object-cover rounded-t-xl group-hover:scale-110 transition duration-300'
             />
             <div className='bg-gray-900 text-white p-3 rounded-b-xl max-w-[250px] flex flex-col justify-between min-h-[100px] md:min-h-[120px] max-h-max group-hover:scale-110 transition duration-300'>
-                <div className='text-sm md:text-lg font-bold bg-green-500'>
+                <div className='text-sm md:text-lg font-bold'>
                     {props.title}
                 </div>
                 <div className='text-xs md:text-sm mt-5'>{props.release}</div>
--- src/pages/TvDetail.jsx
+++ src/pages/TvDetail.jsx
@@ -14,7 +14,7 @@ const TvDetail = () => {
             })
     }, [id])
 
-    console.log(detail)
+    // console.log(detail)
 
     // console.log(detail.name)
     return (
